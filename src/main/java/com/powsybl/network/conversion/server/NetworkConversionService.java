6bee1ea2458022aa4050164af73473cdce72e3e7
Abdelsalem <46495975+AbdelHedhili@users.noreply.github.com>
2020-02-07 15:13:10 +0100

    Use CaseDataSourceClient instead of MemDataSource (#6)
    Signed-off-by: AbdelHedhili <abdelsalem.hedhili@rte-france.com>

--- src/main/java/com/powsybl/network/conversion/server/NetworkConversionService.java
+++ src/main/java/com/powsybl/network/conversion/server/NetworkConversionService.java
@@ -6,8 +6,7 @@
  */
 package com.powsybl.network.conversion.server;
 
-import com.google.common.collect.ImmutableMap;
-import com.powsybl.commons.datasource.ReadOnlyMemDataSource;
+import com.powsybl.cases.datasource.CaseDataSourceClient;
 import com.powsybl.iidm.network.Network;
 import com.powsybl.network.conversion.server.dto.NetworkInfos;
 import com.powsybl.network.store.client.NetworkStoreService;
@@ -15,15 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Objects;
 import java.util.UUID;
@@ -36,8 +29,8 @@ import java.util.UUID;
 @ComponentScan(basePackageClasses = {NetworkStoreService.class})
 public class NetworkConversionService {
 
-    private String caseServerBaseUri;
     private RestTemplate caseServerRest;
+
     private RestTemplate geoDataServerRest;
 
     @Autowired
@@ -46,40 +39,19 @@ public class NetworkConversionService {
     @Autowired
     public NetworkConversionService(@Value("${backing-services.case-server.base-uri:http://case-server/}") String caseServerBaseUri,
                                     @Value("${backing-services.geo-data-server.base-uri:http://geo-data-server/}") String geoDataServerBaseUri) {
-        caseServerRest = new RestTemplateBuilder().build();
+        RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
+        caseServerRest = restTemplateBuilder.build();
         caseServerRest.setUriTemplateHandler(new DefaultUriBuilderFactory(caseServerBaseUri));
-        this.caseServerBaseUri = caseServerBaseUri;
 
-        geoDataServerRest = new RestTemplateBuilder().build();
+        geoDataServerRest = restTemplateBuilder.build();
         geoDataServerRest.setUriTemplateHandler(new DefaultUriBuilderFactory(geoDataServerBaseUri));
     }
 
     NetworkInfos importCase(String caseName) {
-        byte[] networkByte = getCaseAsByte(caseName);
-        String[] baseName = caseName.split("\\.");
-        ReadOnlyMemDataSource readOnlyMemDataSource = new ReadOnlyMemDataSource(baseName[0]);
-        readOnlyMemDataSource.putData(caseName, networkByte);
-        Network network = networkStoreService.importNetwork(readOnlyMemDataSource);
+        CaseDataSourceClient dataSource = new CaseDataSourceClient(caseServerRest, caseName);
+        Network network = networkStoreService.importNetwork(dataSource);
         UUID networkUuid = networkStoreService.getNetworkUuid(network);
         return new NetworkInfos(networkUuid, network.getId());
-    }
-
-    byte[] getCaseAsByte(String caseName) {
-        HttpHeaders requestHeaders = new HttpHeaders();
-        HttpEntity<byte[]> requestEntity = new HttpEntity<>(requestHeaders);
-
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(caseServerBaseUri + "/" + NetworkConversionConstants.CASE_API_VERSION + "/cases/{caseName}")
-                .uriVariables(ImmutableMap.of("caseName", caseName));
-
-        try {
-            ResponseEntity<byte[]> responseEntity = caseServerRest.exchange(uriBuilder.toUriString(),
-                                                                            HttpMethod.GET,
-                                                                            requestEntity,
-                                                                            byte[].class);
-            return responseEntity.getBody();
-        } catch (HttpStatusCodeException e) {
-            throw new NetworkConversionException("getCaseAsByte HttpStatusCodeException", e);
-        }
     }
 
     void setCaseServerRest(RestTemplate caseServerRest) {
