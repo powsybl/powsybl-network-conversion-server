f506ace5aa83d0826b2b86a6b4ee5d29d49df068
Slimane AMAR <slimane.amar@rte-france.com>
2021-11-30 08:30:38 +0100

    Use testcontainers elasticsearch (NodeClientFactoryBean is deprecated) for tests (#56)
    Signed-off-by: Slimane AMAR <slimane.amar@rte-france.com>

--- src/test/java/com/powsybl/network/conversion/server/EmbeddedElasticsearch.java
+++ src/test/java/com/powsybl/network/conversion/server/EmbeddedElasticsearch.java
@@ -1,73 +1,49 @@
-/**
- * Copyright (c) 2021, RTE (http://www.rte-france.com)
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+/*
+  Copyright (c) 2021, RTE (http://www.rte-france.com)
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 package com.powsybl.network.conversion.server;
 
-import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
-import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequestBuilder;
-import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.BoundTransportAddress;
-import org.elasticsearch.common.transport.TransportAddress;
-import org.elasticsearch.http.HttpInfo;
-import org.elasticsearch.index.reindex.ReindexPlugin;
-import org.elasticsearch.node.Node;
-import org.elasticsearch.node.NodeValidationException;
-import org.elasticsearch.transport.Netty4Plugin;
-import org.junit.rules.ExternalResource;
-import org.springframework.data.elasticsearch.client.NodeClientFactoryBean;
 import org.springframework.stereotype.Component;
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
 
-import java.util.List;
-import java.util.UUID;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 /**
  * A class to launch an embedded DB elasticsearch
- *
  * @author Slimane Amar <slimane.amar at rte-france.com>
  */
 @Component
-public class EmbeddedElasticsearch extends ExternalResource {
+public class EmbeddedElasticsearch {
 
-    public EmbeddedElasticsearch() throws NodeValidationException {
-        String pathHome = "src/test/resources/test-home-dir";
-        String pathData = "target/elasticsearchTestData";
-        String clusterName = UUID.randomUUID().toString();
+    private static final String ES_DOCKER_IMAGE_NAME = "docker.elastic.co/elasticsearch/elasticsearch";
+    private static final String ES_DOCKER_IMAGE_VERSION = "7.9.3";
 
-        Node node = new NodeClientFactoryBean.TestNode(
-                Settings.builder()
-                        .put("transport.type", "netty4")
-                        .put("discovery.type", "single-node")
-                        .put("http.type", "netty4")
-                        .put("path.home", pathHome)
-                        .put("path.data", pathData)
-                        .put("cluster.name", clusterName)
-                        .put("node.max_local_storage_nodes", 100)
-                        // the following 3 settings are needed to avoid problems on big, but
-                        // almost full filesystems, see DATAES-741
-                        .put("cluster.routing.allocation.disk.watermark.low", "1gb")
-                        .put("cluster.routing.allocation.disk.watermark.high", "1gb")
-                        .put("cluster.routing.allocation.disk.watermark.flood_stage", "1gb")
-                        .build(),
-                List.of(Netty4Plugin.class, ReindexPlugin.class));
+    private static ElasticsearchContainer elasticsearchContainer;
 
-        node.start();
+    @PostConstruct
+    public void postConstruct() {
+        if (elasticsearchContainer != null) {
+            return;
+        }
 
-        publishNodePort(node);
+        elasticsearchContainer = new ElasticsearchContainer(String.format("%s:%s", ES_DOCKER_IMAGE_NAME, ES_DOCKER_IMAGE_VERSION));
+        elasticsearchContainer.start();
+
+        System.setProperty("spring.data.elasticsearch.embedded", Boolean.toString(true));
+        System.setProperty("spring.data.elasticsearch.embedded.port", Integer.toString(elasticsearchContainer.getMappedPort(9200)));
     }
 
-    private void publishNodePort(Node node) {
-        NodesInfoRequestBuilder nodesInfoRequestBuilder = node.client().admin().cluster().prepareNodesInfo();
-        NodesInfoResponse nodesInfoResponse = nodesInfoRequestBuilder.get();
-        List<NodeInfo> nodeInfos = nodesInfoResponse.getNodes();
-        NodeInfo nodeInfo = nodeInfos.get(0);
-        HttpInfo httpInfo = nodeInfo.getInfo(HttpInfo.class);
-        BoundTransportAddress boundTransportAddress = httpInfo.address();
-        TransportAddress transportAddress = boundTransportAddress.publishAddress();
-        System.setProperty("spring.data.elasticsearch.embedded", Boolean.toString(true));
-        System.setProperty("spring.data.elasticsearch.embedded.port", Integer.toString(transportAddress.getPort()));
+    @PreDestroy
+    private void preDestroy() {
+        if (elasticsearchContainer == null) {
+            return;
+        }
+
+        elasticsearchContainer.stop();
+        elasticsearchContainer = null;
     }
 }
