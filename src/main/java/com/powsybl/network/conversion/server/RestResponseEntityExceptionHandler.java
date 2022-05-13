bb6f8931e0c34ffdf964d5ec4030c55dce268078
sBouzols <sylvain.bouzols@gmail.com>
2022-05-13 13:44:17 +0200

    refactor(): RestResponseEntityExceptionHandler as in modification-server
    Signed-off-by: sBouzols <sylvain.bouzols@gmail.com>

--- src/main/java/com/powsybl/network/conversion/server/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/powsybl/network/conversion/server/RestResponseEntityExceptionHandler.java
@@ -4,15 +4,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-
 package com.powsybl.network.conversion.server;
 
-import org.springframework.beans.TypeMismatchException;
-import org.springframework.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.ServerWebInputException;
 
 /**
  * @author Sylvain Bouzols <sylvain.bouzols at rte-france.com>
@@ -20,26 +18,17 @@ import org.springframework.web.server.ServerWebInputException;
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = { NetworkConversionException.class, TypeMismatchException.class })
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestResponseEntityExceptionHandler.class);
+
+    @ExceptionHandler(value = {NetworkConversionException.class})
     protected ResponseEntity<Object> handleException(RuntimeException exception) {
-        if (exception instanceof NetworkConversionException) {
-            NetworkConversionException conversionException = (NetworkConversionException) exception;
-            switch (conversionException.getType()) {
-                case UNSUPPORTED_FORMAT:
-                case UNKNOWN_EQUIPMENT_TYPE:
-                case UNKNOWN_VARIANT_ID:
-                case FAILED_NETWORK_SAVING:
-                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(conversionException.getMessage());
-                default:
-            }
-        } else if (exception instanceof ServerWebInputException) {
-            ServerWebInputException serverWebInputException = (ServerWebInputException) exception;
-            Throwable cause = serverWebInputException.getCause();
-            if (cause instanceof TypeMismatchException && cause.getCause() != null && cause.getCause() != cause) {
-                cause = cause.getCause();
-                return ResponseEntity.status(serverWebInputException.getStatus()).body(cause.getMessage());
-            }
+        if (LOGGER.isErrorEnabled()) {
+            LOGGER.error(exception.getMessage(), exception);
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        var networkConversionException = (NetworkConversionException) exception;
+        return ResponseEntity
+                .status(networkConversionException.getType().getStatus())
+                .body(networkConversionException.getMessage());
     }
 }
+
