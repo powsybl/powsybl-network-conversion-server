299df39b4d77500ca2d262cd4fe22cb6273a360d
LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>
2022-09-02 11:32:12 +0200

    fix: linebreak in method signature
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier@rte-france.com>

--- src/main/java/com/powsybl/network/conversion/server/NetworkConversionService.java
+++ src/main/java/com/powsybl/network/conversion/server/NetworkConversionService.java
@@ -105,7 +105,9 @@ public class NetworkConversionService {
     public NetworkConversionService(@Value("${backing-services.case-server.base-uri:http://case-server/}") String caseServerBaseUri,
                                     @Value("${backing-services.geo-data-server.base-uri:http://geo-data-server/}") String geoDataServerBaseUri,
                                     @Value("${backing-services.report-server.base-uri:http://report-server}") String reportServerURI,
-                                    NetworkStoreService networkStoreService, @Lazy EquipmentInfosService equipmentInfosService, NetworkConversionExecutionService networkConversionExecutionService,
+                                    NetworkStoreService networkStoreService,
+                                    @Lazy EquipmentInfosService equipmentInfosService,
+                                    NetworkConversionExecutionService networkConversionExecutionService,
                                     NotificationService notificationService) {
         this.networkStoreService = networkStoreService;
         this.equipmentInfosService = equipmentInfosService;
