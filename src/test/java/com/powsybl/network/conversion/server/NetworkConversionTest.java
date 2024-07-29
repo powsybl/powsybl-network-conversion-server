89f6f18a17d6415673240ee5a697037bef776ddc
FranckLecuyer <47824306+FranckLecuyer@users.noreply.github.com>
2024-07-29 12:00:09 +0200

    Upgrade to powsybl dependencies 2024.2.0 (#145)
    Signed-off-by: Franck LECUYER <franck.lecuyer@rte-france.com>

--- src/test/java/com/powsybl/network/conversion/server/NetworkConversionTest.java
+++ src/test/java/com/powsybl/network/conversion/server/NetworkConversionTest.java
@@ -373,13 +373,14 @@ public class NetworkConversionTest {
                 .andReturn();
 
         assertTrue(Objects.requireNonNull(mvcResult.getResponse().getHeader("content-disposition")).contains("attachment;"));
-        //assertTrue(Objects.requireNonNull(mvcResult.getResponse().getHeader("content-disposition")).contains("filename*=UTF-8''urn%3Auuid%3Ad400c631-75a0-4c30-8aed-832b0d282e73"));
-        assertTrue(mvcResult.getResponse().getContentAsString().contains("<md:Model.description>SV Model</md:Model.description>\n" +
-                "        <md:Model.version>1</md:Model.version>\n" +
-                "        <md:Model.DependentOn rdf:resource=\"urn:uuid:c2960b34-0a04-4cd1-9c4d-f3112d85ec6c\"/>\n" +
-                "        <md:Model.profile>http://entsoe.eu/CIM/StateVariables/4/1</md:Model.profile>\n" +
-                "        <md:Model.modelingAuthoritySet>powsybl.org</md:Model.modelingAuthoritySet>\n" +
-                "    </md:FullModel>"));
+        assertTrue(Objects.requireNonNull(mvcResult.getResponse().getHeader("content-disposition")).contains("filename*=UTF-8''urn%3Auuid%3Ad400c631-75a0-4c30-8aed-832b0d282e73"));
+        assertTrue(mvcResult.getResponse().getContentAsString().contains("<md:Model.description>CGMES Conformity Assessment: 'MicroGridTestConfiguration....BC (MAS BE) Test Configuration. The model is owned by ENTSO-E and is provided by ENTSO-E â\u0080\u009Cas it isâ\u0080\u009D. To the fullest extent permitted by law, ENTSO-E shall not be liable for any damages of any kind arising out of the use of the model (including any of its subsequent modifications). ENTSO-E neither warrants, nor represents that the use of the model will not infringe the rights of third parties. Any use of the model shall  include a reference to ENTSO-E. ENTSO-E web site is the only official source of information related to the model.</md:Model.description>\n" +
+            "        <md:Model.version>2</md:Model.version>\n" +
+            "        <md:Model.DependentOn rdf:resource=\"urn:uuid:d400c631-75a0-4c30-8aed-832b0d282e73\"/>\n" +
+            "        <md:Model.DependentOn rdf:resource=\"urn:uuid:f2f43818-09c8-4252-9611-7af80c398d20\"/>\n" +
+            "        <md:Model.DependentOn rdf:resource=\"urn:uuid:2399cbd1-9a39-11e0-aa80-0800200c9a66\"/>\n" +
+            "        <md:Model.profile>http://entsoe.eu/CIM/StateVariables/4/1</md:Model.profile>\n" +
+            "        <md:Model.modelingAuthoritySet>http://elia.be/CGMES/2.4.15</md:Model.modelingAuthoritySet>"));
     }
 
     @Test
