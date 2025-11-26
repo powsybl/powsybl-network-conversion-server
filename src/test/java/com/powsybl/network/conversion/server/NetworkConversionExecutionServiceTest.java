3db1b79a7f6aa1591245d30ffad4088d3ab971cf
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2025-11-26 14:23:06 +0100

    resolve sonar issue

--- src/test/java/com/powsybl/network/conversion/server/NetworkConversionExecutionServiceTest.java
+++ src/test/java/com/powsybl/network/conversion/server/NetworkConversionExecutionServiceTest.java
@@ -51,7 +51,7 @@ class NetworkConversionExecutionServiceTest {
             }
 
             @Override
-            public void reset() {
+            public void setValue() {
                 threadLocal.remove();
             }
         });
