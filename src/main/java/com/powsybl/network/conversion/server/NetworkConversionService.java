b90c860fc2454b65668a5041c87a17d6b458e5e4
Etienne Homer <etiennehomer@gmail.com>
2026-01-09 16:02:28 +0100

    Add content-type to downloaded zip
    Signed-off-by: Etienne Homer <etiennehomer@gmail.com>

--- src/main/java/com/powsybl/network/conversion/server/NetworkConversionService.java
+++ src/main/java/com/powsybl/network/conversion/server/NetworkConversionService.java
@@ -294,13 +294,15 @@ public class NetworkConversionService {
             headers.setContentDisposition(ContentDisposition.builder("attachment")
                     .filename(fileName)
                     .build());
+            headers.add(HttpHeaders.CONTENT_TYPE, Files.probeContentType(Path.of(fileName)));
             headers.setContentLength(s3InputStream.response().contentLength());
             return ResponseEntity.ok()
                     .headers(headers)
-                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
                     .body(new InputStreamResource(s3InputStream));
         } catch (NoSuchKeyException e) {
             return ResponseEntity.notFound().build();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 
