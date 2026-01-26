5f41873620b6e54fd3eee45f2bc07d39e3faaf63
basseche <bassel.el-cheikh_externe@rte-france.com>
2026-01-26 16:10:29 +0100

    Add export contentType to notifications
    Signed-off-by: basseche <bassel.el-cheikh_externe@rte-france.com>

--- src/main/java/com/powsybl/network/conversion/server/dto/ExportInfos.java
+++ src/main/java/com/powsybl/network/conversion/server/dto/ExportInfos.java
@@ -1,3 +1,9 @@
+/*
+  Copyright (c) 2026, RTE (http://www.rte-france.com)
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.conversion.server.dto;
 
 import lombok.AllArgsConstructor;
@@ -5,6 +11,10 @@ import lombok.Getter;
 
 import java.util.Map;
 import java.util.UUID;
+
+/**
+ * @author Etienne Homer <etienne.homer at rte-france.com>
+ */
 
 @AllArgsConstructor
 @Getter
