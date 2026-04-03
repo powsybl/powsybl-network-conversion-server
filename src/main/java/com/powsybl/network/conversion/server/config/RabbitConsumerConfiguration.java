03b43c7796dbe0c6cf91aa923557ca10b433e6a1
LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>
2026-04-03 10:16:50 +0200

    checkstyle + author/copyright
    Signed-off-by: LE SAULNIER Kevin <kevin.lesaulnier.pro@gmail.com>

--- src/main/java/com/powsybl/network/conversion/server/config/RabbitConsumerConfiguration.java
+++ src/main/java/com/powsybl/network/conversion/server/config/RabbitConsumerConfiguration.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.conversion.server.config;
 
 import org.springframework.amqp.rabbit.listener.MessageListenerContainer;
@@ -12,6 +18,9 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 @Configuration
 public class RabbitConsumerConfiguration {
     /*
--- src/test/java/com/powsybl/network/conversion/server/config/RabbitConsumerConfigurationTest.java
+++ src/test/java/com/powsybl/network/conversion/server/config/RabbitConsumerConfigurationTest.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2026, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.powsybl.network.conversion.server.config;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +18,9 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * @author Kevin Le Saulnier <kevin.le-saulnier at rte-france.com>
+ */
 class RabbitConsumerConfigurationTest {
     private RabbitConsumerConfiguration configuration;
     private ListenerContainerCustomizer<MessageListenerContainer> customizer;
